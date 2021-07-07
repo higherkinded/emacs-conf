6836847725db3b67bd85363e0dc5b5d767cfc6df
hk <higherkinded@protonmail.com>
2021-07-07 22:26:15 +0000

    Definitions

--- keybindings.el
+++ keybindings.el
@@ -1,5 +1,19 @@
+(require 'lsp)
+
+(defun lsp-enabled () (bound-and-true-p lsp-mode))
+
+(defmacro lspdefcmd (name &rest expressions)
+  `(defun ,name ()
+    (interactive)
+    (if (lsp-enabled) (progn ,@expressions) (message "LSP isn't active"))))
+
+(lspdefcmd find-definition (lsp-find-definition))
+(lspdefcmd find-refs (lsp-find-references))
+
 (evil-leader/set-leader ";")
 (evil-leader/set-key "w" 'evil-window-map)
 (evil-leader/set-key "m" 'treemacs)
 (evil-leader/set-key "q" 'evil-quit)
 (evil-leader/set-key "SPC" 'toggle-truncate-lines)
+(evil-leader/set-key "f" 'find-definition)
+(evil-leader/set-key "u" 'find-refs)
